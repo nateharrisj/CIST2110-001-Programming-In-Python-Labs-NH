c00d03012463acf770d5d64fca342b720b7fdb2f
nateharrisj <harri222@go.stockton.edu>
2023-11-26 20:49:41 -0500

    Lab10

--- Week12/Lab10.py
+++ Week12/Lab10.py
@@ -2,6 +2,8 @@
 # Author: Nate Harris
 
 # Lab 10 will show demonstrate how Dictionary's work in Python.
+import pandas as pd
+from tabulate import tabulate
 
 # 1. Create a dictionary called my_dict with the following key value pairs:
 # Key: "name", Value: "John"
@@ -9,7 +11,7 @@
 # Key: "city", Value: "Trenton"
 # Key: "state", Value: "New Jersey"
 
-my_dict = {"name": "John", "age": 30, "city": "Trenton", "state": "New Jersey"}   
+my_dict = {"name": "John", "age": 30, "city": "Trenton", "state": "New Jersey"}
 
 # 2. Print out the dictionary.
 print(my_dict)
@@ -29,7 +31,7 @@ for key, value in my_dict.items():
 # 5. Add a new key value pair to the dictionary.
 # Key: "country", Value: "USA"
 
-my_dict["country"] = "USA"
+my_dict["Country"] = "USA"
 
 # 6. Print out the dictionary.
 
@@ -37,13 +39,13 @@ print(my_dict)
 
 # 7. Remove the key value pair with the key "city".
 
-del my_dict["city"] 
+my_dict.pop("city")
 
-# 8. Print out the dictionary. 
+# 8. Print out the dictionary.
 
 print(my_dict)
 
-# 9. Create a dictionary called cities with an key as the City name and values as a list that contains the state, population, and country.
+# 9. Create a dictionary called cities with an key as the City name and values as a dictionary that contains the state, population, and country.
 # use the following data:
 # Trenton, New Jersey, 84,913, USA
 # New York City, New York, 8,336,817, USA
@@ -55,25 +57,38 @@ print(my_dict)
 # San Antonio, Texas, 1,547,253, USA
 # San Diego, California, 1,423,851, USA
 
-cities = {"Trenton": ["New Jersey", 84913, "USA"], "New York City": ["New York", 8336817, "USA"], "Los Angeles": ["California", 3979576, "USA"],
-        "Chicago": ["Illinois", 2693976, "USA"], "Houston": ["Texas", 2320268, "USA"], "Phoenix": ["Arizona", 1680992, "USA"], 
-        "Philadelphia": ["Pennsylvania", 1584138, "USA"], "San Antonio": ["Texas", 1547253, "USA"], "San Diego": ["California", 1423851, "USA"]}
+cities = {
+    "Trenton": {"State": "New Jersey", "Population": 84913, "Country": "USA"},
+    "New York City": {"State": "New York","Population": 8336817, "Country": "USA"},
+    "Los Angeles": {"State": "California", "Population": 3979576, "Country": "USA"},
+    "Chicago": {"State": "Illinois", "Population": 2693976, "Country": "USA"},
+    "Houston": {"State":"Texas", "Population": 2320268, "Country": "USA"},
+    "Phoenix": {"State":"Arizona", "Population": 1680992, "Country": "USA"},
+    "Philadelphia": {"State": "Pennsylvania", "Population": 1584138, "Country": "USA"},
+    "San Antonio": {"State": "Texas", "Population": 1547253, "Country": "USA"},
+    "San Diego": {"State": "California", "Population": 1423851, "Country": "USA"},
+}
 
 # 10. Print a table of the data using the pandas library.
-# pip install pandas
+# pip install pandas. This was put at the top of the file.
 
-import pandas as pd
-print(pd.DataFrame.from_dict(cities, orient='index', columns=["State", "Population", "Country"]))
+df = pd.DataFrame(cities).T
+print("\n", df)
+
+# 11. Use the tabulate library to print out the table. pip install tabulate this was put at the top of the file.
+
+col_row_table= tabulate(df, headers="keys", tablefmt="psql")
+print("\n", col_row_table)
+
+# 12. Print out the population for the city of Chicago.
+
+print("\n The population if Chicago is: ", df.at["Chicago", "Population"])
+#print("\n", df.loc["Chicago", "Population"])
+#print(cities["Chicago"].get("Population"))
+# I played around with different ways to get the population, I liked the first one the best.
 
 
-# 11. Use the tabulate library to print out the table.
-# pip install tabulate
-
-
-# 11. Print out the population for the city of Chicago.
-
-
-# 11. **Extra** An alternative to converting a dictionary into a pandas df then tabulate, you can use dictionary unpacking and tabulate.
+# 13. **Extra** An alternative to converting a dictionary into a pandas df then tabulate, you can use dictionary unpacking and tabulate.
 # Dictionary unpacking is a new feature in Python 3.9 and allows you to unpack a dictionary into a list of dictionaries.
 # You can then use tabulate to print out the table.
 
@@ -84,10 +99,25 @@ print(pd.DataFrame.from_dict(cities, orient='index', columns=["State", "Populati
 
 # Transform the data
 
+new_cities = [{"City": city, **info} for city, info in cities.items()]
+
 # Print the table using tabulate
 
+print("\n", tabulate(new_cities, headers="keys", tablefmt="psql"))
 
-# 12. How many cities have a population greater than 1 million? Use a for loop to iterate through the dictionary.
+# 14. How many cities have a population greater than 1 million? Use a for loop to iterate through the dictionary.
 
-# 13.  How many cities are in the USA? Use .items() to get a list of tuples. Use a for loop to iterate through the list of tuples.
+count_cities = 0
+for city in cities:
+    if cities[city]["Population"] > 1000000:
+        count_cities += 1
+print("\n"f"Cities with a population greater than 1 million: {count_cities}")
+
+# 15.  How many cities are in the USA? Use .items() to get a list of tuples. Use a for loop to iterate through the list of tuples.
 # Hint 1: You will need to use the second item in the tuple. The second item is a dictionary. IE. for city, info in cities.items():
+
+count_cities = 0
+for city, info in cities.items():
+    if info["Country"] == "USA":
+        count_cities += 1  
+print("\n"f"Cities in the USA: {count_cities}")
